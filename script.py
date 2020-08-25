383467fc5de63070188c8f1eae6d16f35382ec8f
Noah Christian <noahchristian@Noahs-MacBook-Pro.local>
2020-08-25 09:12:02 -0400

    initial commit

---
+++ script.py
@@ -0,0 +1,13 @@
+import sys
+import requests
+
+#print(sys.version)
+print(sys.executable)
+
+def greet(who_to_greet):
+    test = "test"
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting 
+
+r = requests.get('http://coreyms.com')
+print(r.status_code)
